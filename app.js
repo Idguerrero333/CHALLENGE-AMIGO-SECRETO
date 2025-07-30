8ad61e22802edadcfdb27b027dcaafd25d062287
Idguerrrero333 <idguerrero333@gmail.com>
2025-07-30 17:16:40 -0500

    Agregado feature añadir nombre en cadena con la funcion (agregarAmigo())

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigoSecreto = [];
+let nombreAñadir = '';
+
+function agregarAmigo(){
+   nombreAñadir = document.getElementById('amigo').value    
+   listaAmigoSecreto.push(nombreAñadir);//incluye en la ultima posicion del array el numero generado
+   document.querySelector('#amigo').value = ''
+   console.log(listaAmigoSecreto);
+   return;
+}
