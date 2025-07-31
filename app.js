8d485e4cb7a041eca986b52683937238e82bc210
Idguerrrero333 <idguerrero333@gmail.com>
2025-07-30 19:44:02 -0500

    agregadas excepciones en el input amigo

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigoSecreto = [];
-let nombreAñadir = '';
+let nombreAñadir;
 
 function agregarAmigo(){
-   nombreAñadir = document.getElementById('amigo').value    
-   listaAmigoSecreto.push(nombreAñadir);//incluye en la ultima posicion del array el numero generado
-   document.querySelector('#amigo').value = ''
-   console.log(listaAmigoSecreto);
-   return;
+    nombreAñadir = document.getElementById('amigo').value
+    let estaVacio = nombreAñadir.trim() === ''; //Comparamos si la cadena, después de eliminar los espacios, está completamente vacía
+    let sonNumeros = /\d/.test(nombreAñadir); //Comprueba si hay algun digito en la cadena
+    let esString = typeof nombreAñadir === 'string'; //comprueba si el el valor es una cadena de string
+    let excluyeCaracteres = !/[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/.test(nombreAñadir); 
+    /*
+        ! invierte la respuesta
+        [a-zA-ZáéíóúÁÉÍÓÚñÑ\s] permite letras (mayúsculas/minúsculas), tildes, la "ñ" y espacios en blanco.
+        [^...] significa "cualquier carácter que NO esté en este conjunto".
+        .test() verifica si hay AL MENOS UN carácter que NO sea una letra, tilde, ñ o espacio.
+    */
+
+    console.warn('que tipo de entrada es:');
+    console.log(typeof(nombreAñadir));
+
+    console.warn('esta vacio:')
+    console.log(estaVacio);
+
+    console.warn('contiene numeros:')
+    console.log(sonNumeros);
+
+    console.warn('contiene caracteres:')
+    console.log(excluyeCaracteres);
+
+    if (estaVacio === false && sonNumeros === false && esString && excluyeCaracteres) {
+        listaAmigoSecreto.push(nombreAñadir);
+        document.querySelector('#amigo').value = '';
+        console.log(listaAmigoSecreto)
+    }else {
+        alert('El dato insertado no corresponde a un nombre, \npor favor vuelve a intentarlo');
+    }
+
+    return;
 }
